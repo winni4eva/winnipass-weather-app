323db5fbc0ee33d3615823779ba6ec45ec76250c
Winnipass <adams4lyf@gmail.com>
2019-04-24 14:16:38 +0000

    en(tests): added weather component test

--- src/app/weather/weather.component.spec.ts
+++ src/app/weather/weather.component.spec.ts
@@ -4,13 +4,19 @@ import { WeatherService } from '../weather/weather.service';
 import { WeatherComponent } from './weather.component';
 import { of } from 'rxjs/internal/observable/of';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 describe('WeatherComponent', () => {
   let component: WeatherComponent;
   let fixture: ComponentFixture<WeatherComponent>;
 
-  const mockWeatherService = jasmine.createSpyObj('WeatherService', ['getLocationId']);
-  mockWeatherService.getLocationId.and.returnValue( of() );
+  const router = {
+    navigate: jasmine.createSpy('navigate')
+  };
+
+  const mockWeatherService = jasmine.createSpyObj('WeatherService', ['getLocationId', 'getWeatherReport']);
+  mockWeatherService.getLocationId.and.returnValue( of([{woeid: 1234}]) );
+  mockWeatherService.getWeatherReport.and.returnValue( of({consolidated_weather: [{'applicable_date': '23-01-19'}]}) );
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,6 +24,7 @@ describe('WeatherComponent', () => {
       schemas: [ NO_ERRORS_SCHEMA ],
       providers: [
         { provide: WeatherService, useValue: mockWeatherService },
+        { provide: Router, useValue: router },
       ],
       imports: [
           RouterTestingModule.withRoutes([]),
@@ -32,7 +39,20 @@ describe('WeatherComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create component', () => {
-    expect(component).toBeTruthy();
+  it('should get weather report when location details are given', () => {
+    spyOn(component, 'handleWeatherResponse').and.returnValue(undefined);
+
+    component.getLocationId('London');
+
+    expect(mockWeatherService.getLocationId).toHaveBeenCalled();
+    expect(mockWeatherService.getWeatherReport).toHaveBeenCalled();
+    expect(component.handleWeatherResponse).toHaveBeenCalledTimes(1);
+  });
+
+  it('should route to single weather details', () => {
+
+    component.getWeatherDetails(1234);
+
+    expect(router.navigate).toHaveBeenCalledWith(['weather', 1234]);
   });
 });
